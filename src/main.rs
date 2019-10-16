c08d3cb43725b8314f27e029a129150f212f6a65
Wasim Dhamriya <56508640+wasimdhamriya@users.noreply.github.com>
2019-10-17 04:13:46 +0500

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,3 @@
-/// Accept twointeger values form the user
-///
-/// main.rs
-/// 
-/// # Examples
-///
-/// ```
-/// use std::io;
-/// extern crate check;
-/// use check::check as chk;
-/// fn main() {
-///     println!("Please Enter Numerator");
-///     let mut Numerator = String::new();
-///     io::stdin().read_line(&mut Numerator);
-///     let Numerator:i32 = Numerator.trim().parse().unwrap();
-///
-///     println!("Please Enter Denominator");
-///     let mut Denominator = String::new();
-///     io::stdin().read_line(&mut Denominator);
-///     let Denominator:i32 = Denominator.trim().parse().unwrap();
-///    
-///     chk::check(Numerator,Denominator);
-///     
-/// }
-/// ```
 use std::io;
 extern crate check;
 use check::check as chk;
